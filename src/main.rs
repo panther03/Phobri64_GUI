2335977eba84c179e3e5d4f75f83d98b750426cb
Julien de Castelnau <tripod320@gmail.com>
2023-02-10 21:34:43 -0600

    trying to make something kinda functional

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use eframe::egui;
 use hidapi::{HidApi, HidDevice};
-use std::time::Instant;
+use std::{time::Instant, f32::consts::E};
 
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
@@ -18,6 +18,8 @@ fn main() {
     let my_app = MyApp {
         cal_step: None,
         usb_handle: dev,
+        stick_x: 0,
+        stick_y: 0
     };
 
     eframe::run_native(
@@ -30,6 +32,8 @@ fn main() {
 struct MyApp {
     cal_step: Option<u32>,
     usb_handle: HidDevice,
+    stick_x: u16,
+    stick_y: u16
 }
 
 // impl Default for MyApp {
@@ -42,24 +46,53 @@ struct MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let arr: &mut[u8] = &mut [0; 16];
+        self.usb_handle.read_timeout(arr, 1).unwrap();
+        dbg!(arr[0]);
+        if arr[0] == 0x69 {
+            if let Some(x) = self.cal_step {
+                self.cal_step = Some(x+1);
+            }
+        } else if arr[0] == 0x68 {
+            if let Some(x) = self.cal_step {
+                self.cal_step = Some(x-1);
+            }
+        } else {
+            self.stick_x = arr[1] as u16;
+            self.stick_y = arr[2] as u16;
+        }
+
         egui::CentralPanel::default().show(ctx, |ui| {
             match &self.cal_step {
                 None => { ui.label("Wait"); }
                 Some(x) => { ui.label(format!("Cal Step: {}", x)); }
             }
+            ui.label(format!("X: {}", self.stick_x));
+            ui.label(format!("Y: {}", self.stick_y));
             if ui.button("Calibration").clicked() {
                 self.usb_handle.send_feature_report(&[0x01, 0x69]).unwrap();
             }
 
-            if ui.button("Get state").clicked() {
-                let now = Instant::now();
-                let mut arr: &mut[u8] = &mut [0; 1024];
-                self.usb_handle.get_feature_report(&mut arr).unwrap();
-                //dbg!(&arr);
-                self.cal_step = Some(*(arr.get(1).unwrap()) as u32);
-                let elapsed = now.elapsed();
-                println!("Elapsed: {:.2?}", elapsed);
-            }
+            // if ui.button("Get state").clicked() {
+            //     let now = Instant::now();
+            //     let mut arr: &mut[u8] = &mut [0; 16];
+            //     self.usb_handle.get_feature_report(&mut arr).unwrap();
+            //     //dbg!(&arr);
+            //     self.cal_step = Some(*(arr.get(1).unwrap()) as u32);
+            //     let elapsed = now.elapsed();
+            //     println!("Elapsed: {:.2?}", elapsed);
+            // }
+
+            // if ui.button("Read controller's report").clicked() {
+            //     let now = Instant::now();
+            //     let mut arr: &mut[u8] = &mut [0; 12];
+            //     self.usb_handle.read_timeout(arr, 1).unwrap(); // 1 millisecond
+            //     dbg!(&arr);
+            //     let elapsed = now.elapsed();
+            //     println!("Elapsed: {:.2?}", elapsed);
+            // }
+            
         });
     }
 }
+
